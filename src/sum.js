454201178dcf10d8a991d037d693f96b35433d50
Pablo Perez <alu0101318318@ull.edu.es>
2022-03-12 19:13:17 +0000

    Docs + package.json

--- src/sum.js
+++ src/sum.js
@@ -1,5 +1,25 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Pablo Pérez González
+ * @author Gabriel Lorenzo Pérez González
+ * @since Mar 12 2022
+ * 
+ * @desc Sum two numbers
+ *
+ */
 
+'use strict';
 
+/**
+ * Sum two numbers and return the result
+ * @param {Number} number1 First operand
+ * @param {Number} number2 Second operand
+ * @returns {Number} The sum of the operands
+ */
 function sumNumbers(number1, number2) {
   return number1 + number2;
 }
--- tests/sum.spec.js
+++ tests/sum.spec.js
@@ -1,3 +1,18 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Pablo Pérez González
+ * @author Gabriel Lorenzo Pérez González
+ * @since Mar 12 2022
+ * 
+ * @desc Test of Sum two numbers
+ *
+ * @see {@link https://jestjs.io/docs/getting-started}
+ */
+
 const sumNumbers = require('../src/sum');
 
 describe('Sum', () => {
