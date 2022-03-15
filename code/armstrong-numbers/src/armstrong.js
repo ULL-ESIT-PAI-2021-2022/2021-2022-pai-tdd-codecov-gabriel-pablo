b214117a8a8094c768f399e1769aa245d43aa0bd
Lorenzo Gabriel Pérez González <alu0101233499@ull.edu.es>
2022-03-15 14:55:05 +0000

    Programa para el coverage completado

---
+++ code/armstrong-numbers/src/armstrong.js
@@ -0,0 +1,85 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Pablo Pérez González
+ * @author Lorenzo Gabriel Pérez González
+ * @since Mar 15 2022
+ *
+ * @desc Armstrong Numbers:
+ *       This program tells if a number is an Armstrong Number.
+ *       This property means that a number is equal to the sum
+ *       of its digits each of them raised to the power of the 
+ *       number of digits.
+ *
+ */
+
+'use strict';
+
+/**
+ * Function that determines if a number is an Armstrong number.
+ * @param {Number} number
+ * @param {Number} armstrongValue
+ * @returns {Number}
+ */
+function isArmstrongNumber(number) {
+  if (number === armstrongValue(number)) {
+    return true;  
+  } else {
+    return false;  
+  }
+}
+
+/**
+ * Calculates how many digits has a specified number.
+ * @param {Number} number
+ * @returns {Number}
+ */
+function calcDigits(number) {
+  let dividedNumber = number;
+  let digits = 1;
+
+  while (dividedNumber >= 10) {
+    dividedNumber = Math.floor(dividedNumber / 10);
+    digits++;
+  }
+
+  return digits;
+}
+
+/**
+ * Implements the Armstrong formula.
+ * @param {Number} number
+ * @returns {Number}
+ */
+function armstrongValue(number) {
+  const digits = calcDigits(number);
+  let remainderNumber;
+  let dividedNumber = number;
+  let result = 0;
+
+  for (let i = 0; i < digits; i++) {
+    remainderNumber = dividedNumber % 10;
+    dividedNumber = Math.floor(dividedNumber / 10);
+    result += remainderNumber ** digits;
+  }
+
+  return result;  
+}
+
+/**
+ * This function takes a value as parameter and checks if the 
+ * type is a number. If it is, it returns the value.
+ * @param {Number} value
+ * @returns {Number}
+ */
+function checkValue(value) {
+  if (typeof(value) !== 'number') {
+    return 'Valor no permitido';
+  }
+  return value;
+}
+
+module.exports = {checkValue, armstrongValue, calcDigits, isArmstrongNumber}
---
+++ code/armstrong-numbers/tests/armstrong.spec.js
@@ -0,0 +1,46 @@
+
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Pablo Pérez González
+ * @author Lorenzo Gabriel Pérez González
+ * @since Mar 15 2022
+ *
+ * @desc Test of Armstrong Numbers
+ *
+ * @see {@link https://jestjs.io/docs/getting-started}
+ */
+
+const armstrong = require('../src/armstrong');
+
+const checkValue = armstrong.checkValue;
+const calcDigits = armstrong.calcDigits;
+const armstrongValue = armstrong.armstrongValue;
+const isArmstrongNumber = armstrong.isArmstrongNumber;
+
+describe('Armstrong numbers', () => {
+  test('Numbers are accepted', () => {
+    expect(checkValue(4)).toEqual(4);  
+  });
+  test('Strings are not accepted', () => {
+    expect(checkValue('4')).toEqual('Valor no permitido');  
+  });
+  test('Tells how many digits has a number', () => {
+    expect(calcDigits(1)).toEqual(1);
+    expect(calcDigits(10)).toEqual(2);
+    expect(calcDigits(1000000)).toEqual(7);
+  });
+  test('Calculates with the Armstrong Formula', () => {
+    expect(armstrongValue(10)).toEqual(1);  
+    expect(armstrongValue(153)).toEqual(153);
+  });
+  test('Checks if a number is an Armstrong number', () => {
+    expect(isArmstrongNumber(9)).toEqual(true);
+    expect(isArmstrongNumber(10)).toEqual(false);
+    expect(isArmstrongNumber(153)).toEqual(true);
+    expect(isArmstrongNumber(154)).toEqual(false);
+  });
+});
