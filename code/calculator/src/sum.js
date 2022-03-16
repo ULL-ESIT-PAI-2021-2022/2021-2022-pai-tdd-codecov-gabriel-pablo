5988d153b3c8e3d574d5fe96709b11426cd8fa3c
Pablo Perez <alu0101318318@ull.edu.es>
2022-03-16 18:22:02 +0000

    Testing CI

--- code/calculator/src/sum.js
+++ code/calculator/src/sum.js
@@ -15,10 +15,10 @@
 'use strict';
 
 /**
- * Sum two numbers and return the result
- * @param {Number} number1 First operand
- * @param {Number} number2 Second operand
- * @returns {Number} The sum of the operands
+ * Sum two numbers and return the result.
+ * @param {Number} number1 First operand.
+ * @param {Number} number2 Second operand.
+ * @returns {Number} The sum of the operands.
  */
 function sumNumbers(number1, number2) {
   if (isNaN(number1) || isNaN(number2)) {
